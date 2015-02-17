ba6425d99481c207179a27d0d267b5ba1814e60b
Andrew Dickinson <fortknoxcomputer@gmail.com>
2015-02-17 09:19:49 -0500

    Added Dad's planar information

--- src/org/usfirst/frc/team3546/robot/commands/SetupToGrabFromTheStep.java
+++ src/org/usfirst/frc/team3546/robot/commands/SetupToGrabFromTheStep.java
@@ -6,14 +6,15 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
  * Requires the following:
  * 	1. The robot be on the edge of the landfill, as close as possible
  *  2. The Arm be in the maximum position
- *  3. The Wrist be down and the carriage be all the way back, as to further #1
+ *  3. The Wrist be up, claw open and the carriage be all the way back, as to further #1
  */
 public class SetupToGrabFromTheStep extends CommandGroup {
     
     public  SetupToGrabFromTheStep() {
     	addParallel(new VeryShortRangeDriveForward()); //Drive in to the landfill zone
-    	addParallel(new SetClawCylinderOpen()); //Sets the claw to open
-    	addParallel(new SetWristCylinderHorizontal());
+    	//These can actually be done in the setup period by the drivers
+//    	addParallel(new SetClawCylinderOpen()); //Sets the claw to open
+//    	addParallel(new SetWristCylinderHorizontal());
     	addParallel(new MoveCarriageToFront());
     	addSequential(new SetArmToStepLevel());
     	//At this point, we should have our claw around the recycling container
